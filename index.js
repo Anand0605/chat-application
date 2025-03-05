38f40f6a8e9c2389bb6b7a01d20c4ded8b783487
Anand Kumar Gautam <anandvns0510@gmail.com>
2025-03-05 23:53:15 +0530

    update

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const http = require("http")
-const express = require("express")
-const path = require("path")
-const Server = require("socket.io")
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io"); // Correct import
 
-const app = express()
-const server = http.createServer(app)
-const io = new Server(server)
+const app = express();
+const server = http.createServer(app); // Create HTTP server correctly
+const io = new Server(server); // Pass the actual server instance
 
+// Socket.io connection event
+io.on("connection", (socket) => {
+    console.log("A new user has connected", socket.id);
+});
 
-io.on("connection", (socket)=>{
-    console.log("a new user has connected ",socket.id)
-})
-app.use(express.static(path.resolve("./public")))
+app.use(express.static(path.resolve("./public")));
 
-app.get("/",(req, res)=>{
-    return res.sendFile("./public/index.html")
-})
+app.get("/", (req, res) => {
+    return res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
-server.listen(9000,()=>console.log(`server is started at PORT:9000`))
+server.listen(9000, () => console.log(`Server is started at PORT: 9000`));
